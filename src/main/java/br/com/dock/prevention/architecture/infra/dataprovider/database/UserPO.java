5663390a4c72246a84821ef2941fb2ab74cc5e1a
Elton Fontes <elton.fontes@dock.tech>
2023-05-18 17:11:28 -0300

    rename package

---
+++ src/main/java/br/com/dock/prevention/architecture/infra/dataprovider/database/UserPO.java
@@ -0,0 +1,22 @@
+package br.com.dock.prevention.architecture.infra.dataprovider.database;
+
+import br.com.dock.prevention.architecture.domain.entity.User;
+import jakarta.persistence.*;
+import lombok.Value;
+
+@Entity
+@Table(schema = "user")
+@Value
+// TODO ANALISAR REGRA PARA DEIXAR ESSE PACOTE VISIVEL SOMENTE PARA O ADAPTER
+public class UserPO {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    Long id;
+    String name;
+    int age;
+
+    public User toUser() {
+        return new User(this.name, this.age);
+    }
+}
